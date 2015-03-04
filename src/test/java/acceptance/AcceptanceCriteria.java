c9677f85925df62efd55da9c6757d3c84c0822a0
Joe Lea <jlea@palantir.com>
2015-03-04 13:33:02 +0000

    Acceptance criteria compiles and is ignored

--- src/test/java/acceptance/AcceptanceCriteria.java
+++ src/test/java/acceptance/AcceptanceCriteria.java
@@ -1,10 +1,19 @@
 package acceptance;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import accounts.Account;
 
 public class AcceptanceCriteria {
     
+    private Clock clock;
+    private Account account;
+    private Printer printer;
+
+    @Ignore
     @Test public void
     account_should_be_able_to_print_out_a_bank_statement_with_the_transactions_in_reverse_chronological_order() {
         setToday("01/04/2014");
@@ -23,5 +32,9 @@ public class AcceptanceCriteria {
         verify(printer).printLine("02/04/2014 | -100 | 900");
         verify(printer).printLine("01/04/2014 | 1000 | 1000");
 
+    }
+
+    private void setToday(String date) {
+        when(clock.today()).thenReturn(date);
     }
 }
