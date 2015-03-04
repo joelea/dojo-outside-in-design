a7a5c2ece921eaee5683c10c4a08b8399ed85aa4
Joe Lea <jlea@palantir.com>
2015-03-04 13:30:43 +0000

    Make mocking out the clock easier

--- src/test/java/acceptance/AcceptanceCriteria.java
+++ src/test/java/acceptance/AcceptanceCriteria.java
@@ -7,13 +7,13 @@ public class AcceptanceCriteria {
     
     @Test public void
     account_should_be_able_to_print_out_a_bank_statement_with_the_transactions_in_reverse_chronological_order() {
-        clock.setToday("01/04/2014");
+        setToday("01/04/2014");
         account.deposit(1000);
 
-        clock.setToday("02/04/2014");
+        setToday("02/04/2014");
         account.withdraw(100);
 
-        clock.setToday("01/05/2014");
+        setToday("01/05/2014");
         account.deposit(500);
         
         account.printStatement();
